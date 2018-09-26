38dc11872dee388bb703181d3e564a610fe2f05f
hervelee <43277017+hervelee@users.noreply.github.com>
2018-09-26 18:23:44 +0200

    Add files via upload

---
+++ bmfws.js
@@ -0,0 +1,82 @@
+// Pop up apres 10s:
+
+setTimeout(function myPopUp (){
+
+    var r = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!");
+        
+        if (r == true) {
+              window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            } 
+        
+        else {
+             } 	
+          	
+          	},10000);
+
+// bouton en mode psy:
+
+function myFunction() {
+    
+    let elmt = document.querySelector("body");
+     elmt.style.backgroundColor= "magenta";
+    
+    let p = document.querySelector("body");
+     elmt = p.getElementsByTagName("p");
+     elmt[0].style.color = "blue";
+     elmt[1].style.color = "blue";
+     elmt[2].style.color = "blue";
+     elmt[3].style.color = "blue";
+     elmt[4].style.color = "blue";
+     elmt[5].style.color = "blue";
+     elmt[6].style.color = "blue";
+     elmt[7].style.color = "blue";
+     elmt[8].style.color = "blue";
+     elmt[9].style.color = "blue";
+
+    let pbis = document.querySelector("body");
+     elmt = pbis.getElementsByTagName("p");
+     elmt[0].style.fontFamily = 'Helvetica';
+     elmt[1].style.fontFamily = 'papyrus';
+     elmt[2].style.fontFamily = 'papyrus';
+     elmt[3].style.fontFamily = 'papyrus';
+     elmt[4].style.fontFamily = 'papyrus';
+     elmt[5].style.fontFamily = 'papyrus';
+     elmt[6].style.fontFamily = 'papyrus';
+     elmt[7].style.fontFamily = 'papyrus';
+     elmt[8].style.fontFamily = 'papyrus';
+     elmt[9].style.fontFamily = 'papyrus';
+
+    let H1 = document.querySelector("body"); 
+     elmt = H1.getElementsByTagName("h1");
+     elmt[0].style.color = "green";
+     elmt[0].style.fontFamily = 'Comic Sans';
+     
+    let H2 = document.querySelector("body"); 
+     elmt = H2.getElementsByTagName("h2");
+     elmt[0].style.color = "green";
+     elmt[1].style.color = "green";
+     elmt[2].style.color = "green";
+     elmt[3].style.color = "green";
+     elmt[4].style.color = "green";
+     elmt[5].style.color = "green";
+
+     elmt[0].style.fontFamily = 'Comic Sans';
+     elmt[1].style.fontFamily = 'Comic Sans';
+     elmt[2].style.fontFamily = 'Comic Sans';
+     elmt[3].style.fontFamily = 'Comic Sans';
+     elmt[4].style.fontFamily = 'Comic Sans';
+     elmt[5].style.fontFamily = 'Comic Sans';
+
+     let H3 = document.querySelector("body"); 
+     elmt = H3.getElementsByTagName("h3");
+     elmt[0].style.color = "green";
+     elmt[1].style.color = "green";
+     elmt[2].style.color = "green";
+
+     elmt[0].style.fontFamily = 'Comic Sans';
+     elmt[1].style.fontFamily = 'Comic Sans';
+     elmt[2].style.fontFamily = 'Comic Sans';
+     
+
+    
+ }
